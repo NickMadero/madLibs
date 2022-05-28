5c1d2f96b4c27accee44560f80963fc74019303b
NickMadero <madero22@students.rowan.edu>
2022-05-28 19:31:54 -0400

    completion and made some changes to words and debugging errors.

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ from random import randint
 import copy
 
 story = (
-        "On a random day my " + {} + " and i decided to go to the beach and was a " + {} + " kind of day " +
-        "we got to the beach and we saw " + {} + " waves" +
-        "after spending some time in the water we got out and ate " + {} + " and drank " + {} + " it was a good day"
+        "On a random day my {} and i decided to go to the beach and was a {} kind of day\n" +
+        "we got to the beach and we saw {} waves\n" +
+        "after spending some time in the water we got out and ate {} and drank {} it was a good day\n"
 )
 
 word_dict = {
     'person': ['dog', 'girlfriend', 'friend'],
-    'adjective': ['hot', 'gloomy', 'dark', 'burning'],
-    'adjective': ['huge', 'massive', 'large', 'small'],
+    'adjective': ['hot', 'gloomy', 'dark', 'bright'],
+    'adjective2': ['huge', 'massive', 'large', 'small'],
     'food': ['hotdogs', 'burgers', 'fries'],
     'drink': ['soda', 'water', 'juice']
 
@@ -24,4 +24,13 @@ def grab_word(type,local_dict):
 
 def story_function():
     local_dict = copy.deepcopy(word_dict)
-    return story.format()
+    return story.format(
+        grab_word('person',local_dict),
+        grab_word('adjective',local_dict),
+        grab_word('adjective2',local_dict),
+        grab_word('food',local_dict),
+        grab_word('drink',local_dict)
+
+    )
+print("madlibs! ")
+print(story_function())
